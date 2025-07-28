f35638591ef25e0068bf9550ddf5c8c9b44a1d50
@ramprasanth_e <ramprasanth_e@epam.com>
2025-07-28 21:45:23 +0530

    frontend setup done

---
+++ frontend/src/components/UserInput.jsx
@@ -0,0 +1,12 @@
+function UserInput(){
+    return(
+        <div>
+            <label>
+                Your Unhinged Queries: <input name="query"/>
+            </label>
+            <button name='submit'>Go!</button>
+        </div>
+    )
+}
+
+export default UserInput
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import UserInput from "../components/UserInput"
+
+function Home(){
+    return (
+    <>
+      <div>
+        <h1>Agent Space</h1>
+      </div> 
+      <UserInput/>
+    </>
+    )
+}
+
+export default Home

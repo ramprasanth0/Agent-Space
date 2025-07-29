8d4f717a4434f50c5362c24675b05c1d5248c49c
@ramprasanth_e <ramprasanth_e@epam.com>
2025-07-29 18:02:31 +0530

    refactored all styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import UserInput from "../components/UserInput"
 
 export default function Home(){
     return (
-    <div className="min-h-screen">
+    <div>
       <header className="pt-10 pb-64 flex justify-center">
         <h1 className="text-4xl md:text-4xl lg:text-5xl font-extrabold text-indigo-700">Agent Space</h1>
       </header>
